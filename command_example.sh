8eceada933ca5864b9ed5da6141a3db60ad01ab4
running-terminal-commands <runningterminalcommands@gmail.com>
2025-12-05 02:11:59 +0900

    開発用リポジトリにfirst commit

---
+++ command_example.sh
@@ -0,0 +1,14 @@
+# ./command_example.sh
+
+
+cd npm_default
+cd workDirectory_npm_default
+npm init -y
+npm run test
+
+npm install ../install_npm_default
+node index.js
+
+
+#npm installの引数に指定したフォルダにpackage.jsonが無ければインストールできない
+
